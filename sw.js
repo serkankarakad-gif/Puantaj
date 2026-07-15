298e44b0584818f11e4411b07cc576326dca4adb
serkankarakad-gif <serkankarakad@gmail.com>
2026-07-15 19:49:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Puantaj Defterim — service worker (çevrimdışı kabuk) */
-const KASA = "puantaj-v21";
+const KASA = "puantaj-v26";
 const DOSYALAR = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", e => {
